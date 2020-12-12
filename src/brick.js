4f93a8df7a913c22527a18d9e630f263724de442
Awshaf Ishtiaque <aishtiaq@sfu.ca>
2020-12-12 11:20:35 +0600

    first brick created

---
+++ src/brick.js
@@ -0,0 +1,22 @@
+export default class Brick{
+    constructor(game, position){
+        this.GAMEWIDTH = game.GAMEWIDTH;
+        this.GAMEHEIGHT = game.GAMEHEIGHT;
+
+        // this.position = position;
+   
+        this.position = position;
+
+
+        this.width = 80;
+        this.height = 25;
+
+        console.log(`brick created:{${this.position.x},${this.position.y}}`);
+    }
+
+    draw(ctx){
+        ctx.fillStyle = 'red';
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    }
+}
--- src/game.js
+++ src/game.js
@@ -1,6 +1,8 @@
 import Ball from './ball.js';
 import Paddle from './paddle.js';
 import InputHandler from  './input.js';
+import Brick from './brick.js';
+import * as Levels from './levels.js';
 
 export default class Game{
     
@@ -17,6 +19,23 @@ export default class Game{
 
         new InputHandler(this.paddle, this);
 
+
+        //bricks
+        // var bricks = [];
+        // for ( var i = 0; i< 10; i ++){
+        //     //
+        // }
+        var brickPositions = [];
+        const levels = Levels.level1;
+        
+        var tempPosition = {
+            x:30,
+            y:40
+        };
+
+        this.brick = new Brick(this, tempPosition );
+   
+
     }
 
     update(deltaTime){
@@ -28,5 +47,14 @@ export default class Game{
         this.paddle.draw(ctx);
         this.ball.draw(ctx);
         
+
+        //brick:
+        this.position = {
+            x:30,
+            y:40
+        }
+
+        this.brick.draw(ctx);
+
     }
 }
